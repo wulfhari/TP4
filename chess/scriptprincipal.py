4621e774c4a606c87677b970e545d8161bf6433a
wulfhari <tols0001@uqar.ca>
2013-11-04 15:44:13 -0500

    lundi4_15:45

--- chess/scriptprincipal.py
+++ chess/scriptprincipal.py
@@ -6,3 +6,13 @@ Created on Nov 4, 2013
 
 if __name__ == '__main__':
     pass
+
+board = []
+for x in range(0, 5):
+    board.append(["O"] * 5)
+
+def print_board(board):
+    for row in board:
+        print " ".join(row)
+
+print_board(board)
