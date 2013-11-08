e9f94382df26ad5d786ab7e0177992e75c6495de
wulfhari <tols0001@uqar.ca>
2013-11-08 14:55:16 -0500

    classefou

---
+++ chess/fou.py
@@ -0,0 +1,17 @@
+'''
+Created on Nov 8, 2013
+
+@author: Simon
+'''
+
+class fou(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self:
+        '''
+        Constructor
+        '''
+        
