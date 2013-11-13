c90e046b473b8adf87294475c03b33e1a953b743
Zotol <admin@MacBook-Pro-de-Admin-2.local>
2013-11-13 18:53:58 -0500

    classe piece et cavalier

--- chess/cavalier.py
+++ chess/cavalier.py
@@ -14,17 +14,13 @@ class cavalier:
     
     def __init__(self,line,col,couleur,pos):
         """Initialise un cavalier à la position (ligne,colonne) avec la bonne couleur 0 pour noir, 1 pour blanc"""
-        Piece.__init__(self,line,col,couleur,pos)
-        self.line = line
-        self.col = col
-        self.couleur = couleur
-        self.pos = [self.line, self.col]
+        Piece.__init__(self,line,col,couleur)
     
     def deplacer(self,n_line,n_col,plateau):
         if deplacementValide == True:
     
     
-    def deplacementValide(self, nouvPos,plateau):
+    def deplacementValide(self,nouvPos,plateau):
         pos_dep = self.pos
         pos_arr = nouvPos
         #Si une piece a  l'arrivee est identique
