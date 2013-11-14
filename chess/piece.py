6b95b80053532a36411a3975e6f72a60c0e0822a
wulfhari <tols0001@uqar.ca>
2013-11-13 19:49:15 -0500

    blbalba

--- chess/piece.py
+++ chess/piece.py
@@ -3,20 +3,14 @@ Created on Nov 5, 2013
 
 @author: Simon
 '''
-from chess.tour import Tour
-
 class piece(object):
     '''
     classdocs
     '''
 
 
-    def __init__(self,line,col,couleur):
+    def __init__(self, line , col, couleur):
         '''
         Constructor
         '''
-        """Initialise un cavalier à la position (ligne,colonne) avec la bonne couleur 0 pour noir, 1 pour blanc"""
-        self.line = line
-        self.col = col
-        self.couleur = couleur
-        self.pos = [self.line, self.col]
+        
--- chess/tour.py
+++ chess/tour.py
@@ -8,18 +8,15 @@
 Fichier contenant la classe Tour
 """
 
-class Tour:
+class Tour(piece):
     """Une tour est une pièce d'échec qui peut se déplacer selon les ligne et les colonnes"""
     
     def __init__(self,line,col,couleur):
         """Initialise une tour à la position (ligne,colonne) avec la bonne couleur 0 pour noir, 1 pour blanc"""
-        pass
+        self.pos = [line, col]
+        self.color = couleur
+        self.nouvPos = []
     
-    def deplacer(self,n_line,n_col,plateau):
-        if deplacementValide == True:
-            
-        
-       
     def deplacementValide(self, nouvPos,plateau):
         pos_dep = self.pos
         pos_arr = nouvPos
@@ -37,12 +34,22 @@ class Tour:
                     return False
             return True
         
-        elif ... pour la colone
+        elif (pos_dep[0] == pos_arr[0]):
+            depart = min(pos_dep[1],pos_arr[1])+1
+            arrivee = max(pos_dep[1],pos_arr[1])-1
+            #si une piece est dans le chemin
+            for i in range(depart,arrivee):
+                if plateau.get.Piece(pos_dep[0],i) |- None:
+                    return False
+            return True
+        
+    def deplacer(self,n_line,n_col,plateau):
+        if deplacementValide() == True:
 #-----------------
 # FIN CLASSE TOUR
 #-----------------
 
-def test_pos():
+    def test_pos():
         from chess.plateau import Plateau
         p = Plateau()
         tn1 = p.getPiece(0,0)
@@ -68,9 +75,8 @@ def test_pos():
         p.damier.pop((0,4)) 
         print(tb2.deplacer(0,3,p)) # Ne fais rien
         print(p.damier)
-        
-if __name__ == "__main__":
     
+if __name__ == "__main__":
     test_pos()     
 #    test_pos2()     
         
