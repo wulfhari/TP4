3bb68115df7d3d6aa5f5f9723bbcfff1fcd178c7
Zotol <admin@MacBook-Pro-de-Admin-2.local>
2013-11-14 15:53:49 -0500

    modif script princ

--- chess/scriptprincipal.py
+++ chess/scriptprincipal.py
@@ -11,46 +11,49 @@ if __name__ == '__main__':
     pass
 
     ### Menu principal( jeu par création, jeu par chargement)
-    debut = str(input("Bonjour! Voulez-vous créer une nouvelle partie (NV) ou charger une partie existante (CP)? "))
-    start = True
-
-    while start:
-        if debut.upper() == "NV":
-            new_game(self)
-            start = False
-        elif debut.upper() == "CP":
-            load_game(self)
-            start = False
-        else:
-            print ("Désolé cette entrée est inconnue, veuillez inscrire NV pour une nouvelle partie ou CP pour charger un partie existante")
+    def menu_princ():
         debut = str(input("Bonjour! Voulez-vous créer une nouvelle partie (NV) ou charger une partie existante (CP)? "))
+        start = True
+
+        while start:
+            if debut.upper() == "NV":
+                new_game(self)
+                start = False
+            elif debut.upper() == "CP":
+                load_game(self)
+                start = False
+            else:
+                print ("Désolé cette entrée est inconnue, veuillez inscrire NV pour une nouvelle partie ou CP pour charger un partie existante")
+            debut = str(input("Bonjour! Voulez-vous créer une nouvelle partie (NV) ou charger une partie existante (CP)? "))
 
 
     ### Menu ( jouer le tour, arrêter et sauvegarder)
-    partie = str(input("Que voulez-vous faire? Jouer le tour (J), arrêter partie et sauvegarder la partie (A) ou simplement sauvegarder la partie (S)? "))
-    cont = True
-    while start:
-        if partie.upper() == "J":
-            alternance(self)
-            next_turn(self)
-            cont = False
-        elif partie.upper() == "A":
-            quitter = str(input("Êtes-vous certain de vouloir quitter? (O/N) "))
-                if quitter.upper() == "O":
-                    save_game(self)
-                    quit_game(self)
-                    cont = False
-                elif quitter.upper() == "N":
-                    pass
-                else:
-                    print ("Veuillez inscrire O pour Oui ou N pour Non")
-        elif partie.upper() == "S":
-            save_game(self)
-            cont = False
-        else:
-            print ("Désolé cette entrée est inconnue, veuillez inscrire J pour jouer le prochain tour, A pour arrêter et sauvegarder la partie ou S pour sauvegarder la partie.")
-
+    
+    def menu_tour():
         partie = str(input("Que voulez-vous faire? Jouer le tour (J), arrêter partie et sauvegarder la partie (A) ou simplement sauvegarder la partie (S)? "))
+        cont = True
+        while start:
+            if partie.upper() == "J":
+                alternance(self)
+                next_turn(self)
+                cont = False
+            elif partie.upper() == "A":
+                quitter = str(input("Êtes-vous certain de vouloir quitter? (O/N) "))
+                    if quitter.upper() == "O":
+                        save_game(self)
+                        quit_game(self)
+                        cont = False
+                    elif quitter.upper() == "N":
+                        pass
+                    else:
+                        print ("Veuillez inscrire O pour Oui ou N pour Non")
+            elif partie.upper() == "S":
+                save_game(self)
+                cont = False
+            else:
+                print ("Désolé cette entrée est inconnue, veuillez inscrire J pour jouer le prochain tour, A pour arrêter et sauvegarder la partie ou S pour sauvegarder la partie.")
+
+            partie = str(input("Que voulez-vous faire? Jouer le tour (J), arrêter partie et sauvegarder la partie (A) ou simplement sauvegarder la partie (S)? "))
 
     ### affichage ‡ chaque tour
 
