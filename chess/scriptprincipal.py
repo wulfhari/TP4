f613100ddfb877b818061d236dfb03ea289eeb13
Zotol <admin@MacBook-Pro-de-Admin-2.local>
2013-11-14 14:17:56 -0500

    rien de majeur mais si je veux sunchroniser je dois commiter

--- chess/scriptprincipal.py
+++ chess/scriptprincipal.py
@@ -12,15 +12,15 @@ if __name__ == '__main__':
 
     ### Menu principal( jeu par création, jeu par chargement)
     debut = str(input("Bonjour! Voulez-vous créer une nouvelle partie (NV) ou charger une partie existante (CP)? "))
-    start = true
+    start = True
 
     while start:
         if debut.upper() == "NV":
             new_game(self)
-            start = false
+            start = False
         elif debut.upper() == "CP":
             load_game(self)
-            start = false
+            start = False
         else:
             print ("Désolé cette entrée est inconnue, veuillez inscrire NV pour une nouvelle partie ou CP pour charger un partie existante")
         debut = str(input("Bonjour! Voulez-vous créer une nouvelle partie (NV) ou charger une partie existante (CP)? "))
@@ -28,25 +28,25 @@ if __name__ == '__main__':
 
     ### Menu ( jouer le tour, arrêter et sauvegarder)
     partie = str(input("Que voulez-vous faire? Jouer le tour (J), arrêter partie et sauvegarder la partie (A) ou simplement sauvegarder la partie (S)? "))
-    cont = true
+    cont = True
     while start:
         if partie.upper() == "J":
             alternance(self)
             next_turn(self)
-            cont = false
+            cont = False
         elif partie.upper() == "A":
             quitter = str(input("Êtes-vous certain de vouloir quitter? (O/N) "))
                 if quitter.upper() == "O":
                     save_game(self)
                     quit_game(self)
-                    cont = false
+                    cont = False
                 elif quitter.upper() == "N":
                     pass
                 else:
                     print ("Veuillez inscrire O pour Oui ou N pour Non")
         elif partie.upper() == "S":
             save_game(self)
-            cont = false
+            cont = False
         else:
             print ("Désolé cette entrée est inconnue, veuillez inscrire J pour jouer le prochain tour, A pour arrêter et sauvegarder la partie ou S pour sauvegarder la partie.")
 
