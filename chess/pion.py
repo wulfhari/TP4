d04b7b2798cac5eaef7e5c8ec5964ecdf87021bf
slim97 <sim97@msn.com>
2013-11-17 15:42:48 -0500

    Classe Pion

--- chess/pion.py
+++ chess/pion.py
@@ -16,35 +16,95 @@ class Pion(Piece):
     def deplacer(self,nouvPos,plateau):
         if self.deplacementValide(nouvPos,plateau)==True:
             self.pos=nouvPos
+            self.abouge=True
         return self.pos
     
     def deplacementValide(self,nouvPos,plateau):
         pos_dep=self.pos
         pos_arr=nouvPos
         
-        #Si une piece est sur la case d'arrivee
+        #Si une piece alliee est sur la case d'arrivee
         pieceArr=plateau.getPiece(pos_arr[0],pos_arr[1])
+        pieceDev=plateau.getPiece(pos_dep[0]+1,pos_dep[1])
         if (pieceArr != None) and (pieceArr.color == self.color) :
             return False
+
+                
+        #PIONS NOIRS
         
-        ###a revoir, fonctionne juste pour l'equipe qui doit monter        
-        #le pion avance toujours d'une ligne
-        if (pos_dep[0]+1 == pos_arr[0]):
+        if self.color==0:
             
-        #Deplacement en ligne droite de 1 case            
-            if (pos_dep[1]==pos_arr[1]) and pieceArr == None:
+            #Avancer de deux cases a son premier coup
+            if (pos_dep[0]+2==pos_arr[0]) and (pos_dep[1]==pos_arr[1]) and (self.abouge==False) and pieceDev==None :
                 return True
-            
-        #Deplacement en diagonale pour manger une piece
-            elif (pos_dep[1]==pos_arr[1]+1) and pieceArr != None:
-                return True
-            
-            elif (pos_dep[1]==pos_arr[1]-1) and pieceArr != None:
-                return True
-            
-            else:
-                return False
         
+            #les pions avancent toujours d'une seule case
+            if (pos_dep[0]+1 == pos_arr[0]):
+            
+                #Deplacement en ligne droite de 1 case            
+                if (pos_dep[1]==pos_arr[1]) and pieceArr == None:
+                    return True
+            
+                #Deplacement en diagonale pour manger une piece
+                elif (pos_dep[1]==pos_arr[1]+1) and pieceArr != None:
+                    return True
+            
+                elif (pos_dep[1]==pos_arr[1]-1) and pieceArr != None:
+                    return True
+            
+                else:
+                    return False
+            
+        #PIONS BLANCS
+        
+        if self.color==1:
+            
+            #Avancer de deux cases a son premier coup
+            if (pos_dep[0]-2==pos_arr[0]) and (pos_dep[1]==pos_arr[1]) and (self.abouge==False) and pieceDev==None :
+                return True
+        
+            #les pions avancent toujours d'une seule case
+            if (pos_dep[0]-1 == pos_arr[0]):
+            
+                #Deplacement en ligne droite de 1 case            
+                if (pos_dep[1]==pos_arr[1]) and pieceArr == None:
+                    return True
+            
+                #Deplacement en diagonale pour manger une piece
+                elif (pos_dep[1]==pos_arr[1]+1) and pieceArr != None:
+                    return True
+            
+                elif (pos_dep[1]==pos_arr[1]-1) and pieceArr != None:
+                    return True
+            
+                else:
+                    return False
+            
+
         else:
             return False
+        
+def test_pion():
+    from plateau import Plateau
+    p = Plateau()
+    pn1 = p.getPiece(1,0)
+    pn2 = p.getPiece(1,1)
+    pb1 = p.getPiece(6,0)
+    pb2 = p.getPiece(6,1)
+    
+    print(p.damier)
+    pn1.deplacer((2,0),p)
+    pn2.deplacer((3,1),p)
+    pb1.deplacer((4,0),p)
+    pn2.deplacer((4,0),p) #pn2 mange pb1
+    pb2.deplacer((5,1),p)
+    pb2.deplacer((3,1),p) #invalide, avance de 2 a son 2ieme coup
+    
+    
+    
 
+    
+
+
+if __name__=="__main__":
+    test_pion()
