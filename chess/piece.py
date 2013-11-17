f521d80ea75b0035ec5549eeac4b7bbd9db91f36
wulfhari <tols0001@uqar.ca>
2013-11-17 17:58:01 -0500

    stop aux parentheses dans les strings

--- chess/piece.py
+++ chess/piece.py
@@ -13,7 +13,7 @@ class Piece(object):
         '''
         Constructor
         '''
-        """Initialise une piece a  la position (ligne,colonne) avec la bonne couleur 0 pour noir, 1 pour blanc"""
+        """Initialise une piece a  la position 'ligne,colonne' avec la bonne couleur 0 pour noir, 1 pour blanc"""
         self.pos = (line,col) # Sa position
         self.color = couleur # Sa couleur ... il faudra mettre super() si on a une classe Piece mere.
         
