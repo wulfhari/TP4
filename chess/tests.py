7b2939da5af2f9bf35d62a3234740e7e1036bfb4
wulfhari <tols0001@uqar.ca>
2013-11-18 23:50:44 -0500

    transformation

--- chess/tests.py
+++ chess/tests.py
@@ -1,50 +1,16 @@
 #! c:/Python33 python
 # -*- coding:Utf8 -*-
 
-def load_game(file_name):
+def transformation(board):
     
-    from chess.plateau import Plateau
-    from chess.tour import Tour
-    from chess.dame import Dame
-    from chess.roi import Roi
+    from chess.dame import Dame    
     from chess.pion import Pion
-    from chess.fou import Fou
-    from chess.cavalier import Cavalier
-    
-    board = Plateau()
-    board.damier = {}
-    
-    file_name = file_name+".txt"
-    import os
-    cwd = os.getcwd()
-    path = os.path.join(cwd, file_name)
-    
-
-    f = open(path, "r")
-    lines = f.readlines()
-    first_line = lines[0]
-    pieces = {'T':Tour, 'C':Cavalier, 'F':Fou, 'D':Dame, 'R':Roi, 'P':Pion}
-    couleur = {'B':1,'N':0}
-    
-    board.tour = first_line[0]
-    
-    for i in range(1,len(lines)):
-        line = lines[i]
-        board.damier[(int(line[0]),int(line[1]))] = pieces[line[2]](int(line[0]),int(line[1]),couleur[line[3]])
+     
+    for i in range(0,8):
+        if board[0,i] == Pion:
+            board[0,i] = Dame(0,i,board[0,i].color)
+    for i in range(0,8):
+        if board[7,i] == Pion:
+            board[7,i] = Dame(7,i,board[0,i].color)
         
-    
-    if board.damier[(7,7)] != None:    
-        board.damier[(7,7)].abouge = int(first_line[2])
-    elif board.damier[(7,0)] != None:    
-        board.damier[(7,0)].abouge = int(first_line[4])
-    elif board.damier[(0,7)] != None:
-        board.damier[(0,7)].abouge = int(first_line[6])
-    elif board.damier[(0,0)] != None:
-        board.damier[(0,0)].abouge = int(first_line[8])
-    
-    
-    f.close()
-    print(board.damier)
-    return board
-
-load_game("tests_prof\jeuF")
+       
